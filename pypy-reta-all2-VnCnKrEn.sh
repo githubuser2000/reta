9a87a1ba5b3002f2e4f43cc4a2b2bc3ef27aad8b
Alexander Kern <alexgit@alexkern.de>
2025-05-07 09:23:25 +0200

    deploy Skripte vielleicht jetzt besser abbrechbar, mh, und korrigiert

--- pypy-reta-all2-VnCnKrEn.sh
+++ pypy-reta-all2-VnCnKrEn.sh
@@ -5,18 +5,13 @@ ABS_SCRIPT_PATH="$(realpath "${SCRIPT_PATH}")"
 ABS_DIRECTORY="/home/alex/Eigene-Dateien/myRepos/reta"
 GIT_DIRECTORY="${ABS_DIRECTORY}/.git"
 ${ABS_DIRECTORY}/libs/changeVersion.sh
-function ctrl_c() {
-    cat ${ABS_DIRECTORY}/head1.alx
-    cat ${ABS_DIRECTORY}/religionen.js | sed 's/Grundstrukturen/basic_structures/g'
-    cat ${ABS_DIRECTORY}/head2.alx
-    cat ~/middle-${2}.alx
-    pypy3 ${ABS_DIRECTORY}/grundStrukHtml.py blank
-    pypy3 ${ABS_DIRECTORY}/grundStrukHtml.py blank -language=${2}
-    cat ${ABS_DIRECTORY}/footer.alx
-}
-trap ctrl_c INT
 if [ "$1" == 'reta' ]; then
 	pypy3 ${ABS_DIRECTORY}/reta -columns --all --width=0 -output --type=html --onetable --nocolor -language=${2} >  ~/middle-${2}.alx
 fi
+cat ${ABS_DIRECTORY}/head1.alx
+cat ${ABS_DIRECTORY}/religionen.js | sed 's/Grundstrukturen/basic_structures/g'
+cat ${ABS_DIRECTORY}/head2.alx
+cat ~/middle-${2}.alx
+pypy3 ${ABS_DIRECTORY}/grundStrukHtml.py blank -language=${2}
+cat ${ABS_DIRECTORY}/footer.alx
 #chown -R alex:alex ${ABS_DIRECTORY}
-ctrl_c ${1} ${2}
--- pypy-reta-all2-en.sh
+++ pypy-reta-all2-en.sh
@@ -4,17 +4,15 @@ ABS_SCRIPT_PATH="$(realpath "${SCRIPT_PATH}")"
 #ABS_DIRECTORY="$(dirname "${ABS_SCRIPT_PATH}")"
 ABS_DIRECTORY="/home/alex/Eigene-Dateien/myRepos/reta"
 GIT_DIRECTORY="${ABS_DIRECTORY}/.git"
-function ctrl_c() {
-    cat ${ABS_DIRECTORY}/head1.alx
-    cat ${ABS_DIRECTORY}/religionen.js | sed 's/Grundstrukturen/basic_structures/g'
-    cat ${ABS_DIRECTORY}/head2.alx
-    cat ~/middle.alx
-    pypy3 ${ABS_DIRECTORY}/grundStrukHtml.py blank -language=english
-    cat ${ABS_DIRECTORY}/footer.alx
-}
-trap ctrl_c INT
+${ABS_DIRECTORY}/libs/changeVersion.sh
 if [ "$1" == 'reta' ]; then
 	pypy3 ${ABS_DIRECTORY}/reta -columns --all --width=0 -output --type=html --onetable --nocolor -language=english >  ~/middle.alx
 fi
+cat ${ABS_DIRECTORY}/head1.alx
+cat ${ABS_DIRECTORY}/religionen.js | sed 's/Grundstrukturen/basic_structures/g'
+cat ${ABS_DIRECTORY}/head2.alx
+cat ~/middle.alx
+pypy3 ${ABS_DIRECTORY}/grundStrukHtml.py blank -language=english
+cat ${ABS_DIRECTORY}/footer.alx
 #chown -R alex:alex ${ABS_DIRECTORY}
 ctrl_c
